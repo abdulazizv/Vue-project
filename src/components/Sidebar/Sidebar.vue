40ca6c58fcaea9bf1347cde9da9a211f9a7b9d8d
abdulazizv <abdulazizheir@gmail.com>
2023-02-17 10:13:30 +0500

    dynamic contents

--- src/components/Sidebar/Sidebar.vue
+++ src/components/Sidebar/Sidebar.vue
@@ -1,5 +1,17 @@
 <template>
-  <div></div>
+  <div class="bg-indigo-100 w-[300px] min-h-screen shadow-lg">
+      <a href="#" class="text-2xl font-bold bg-white text-center block text-indigo-600">
+        ADMIN DASHBOARD
+      </a>
+
+      <ul class="list">
+        <li class="list__item py-1 px-2">
+          <a href="#" class="flex list__item--link min-w-full items-center bg-white p-4 rounded-lg text-lg shadow border hover:bg-indigo-50 duration-300 font-bold">
+            <i class='bx bxs-dashboard text-indigo-500 text-2xl mr-3'></i> <span class="">NAVLINKS-1</span>
+          </a>
+        </li>
+      </ul>
+  </div>
 </template>
 
 <script>
@@ -9,4 +21,6 @@ export default {
 };
 </script>
 
-<style></style>
+<style >
+
+</style>
