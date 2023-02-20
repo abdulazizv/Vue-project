d1692ee7b7a305332dd8d98d46d77378dc913009
abdulazizv <abdulazizheir@gmail.com>
2023-02-20 10:03:31 +0500

    elements being dynamic

--- src/ui/Input.vue
+++ src/ui/Input.vue
@@ -1,12 +1,17 @@
 <template>
-    <label for="form" class="w-[300px] block my-3">
-        <p class="text-indigo-500 my-2"></p>
-        <input type="text" id="form" class="p-3 w-full ring-2 focus:ring-cyan-700 focus:outline-none placeholder:text-slate-700" placeholder="Enter usernamer">
+    <label :for="labelId" class="w-[300px] block my-3">
+        <p class="text-indigo-500 my-2" v-if="placeholder">{{ placeholder }}</p>
+        <input type="text" :id="labelId" class="p-3 w-full ring-2 focus:ring-cyan-700 focus:outline-none placeholder:text-slate-700" :placeholder="placeholder">
     </label>
 </template>
 <script>
 export default {
-    name:'Button'
+    name:'Button',
+    props: {
+        labelId: String,
+        placeholder: String,
+        inputType : String
+    }
 }
 </script>
 <style lang="scss">
