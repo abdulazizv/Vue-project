94eceb777d0f7654a78ee0b8045cfc0e6e40dc26
abdulazizv <abdulazizheir@gmail.com>
2023-02-20 11:49:49 +0500

    router added

---
+++ src/router/index.js
@@ -0,0 +1,14 @@
+const { createRouter, createWebHashHistory } = require("vue-router");
+import Home from "../views/Home/Home.vue";
+import About from "../views/About/About.vue";
+
+const router = createRouter({
+    history : createWebHashHistory(import.meta.env.BASE_URL),
+    routes:[
+        {path:"/",name:"home" ,component:Home },
+        {path:"/",name:"home",component:About},
+        {path: "/",name:"home",component: Home}
+    ]
+})
+
+export default router;
